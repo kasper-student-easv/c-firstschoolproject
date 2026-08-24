d266cf8df98a51479c09313732189fe2dc25ef37
Kasper <kasand04@easv365.dk>
2026-08-24 11:23:38 +0200

    count duplicet in list

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿public class Program
+﻿using System.Collections;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         //halloWriter();
         //Console.WriteLine(sumTwoNumbers(10, 20));
+        List<int> ints = new List<int>();
+        ints.Add(1);
+        ints.Add(2);
+        ints.Add(2);
+        ints.Add(2);
+        ints.Add(3);
+        ints.Add(3);
+        Console.WriteLine(NumberOfDuplicatesInList(ints));
     }
 
 
@@ -16,6 +26,27 @@
     private static int sumTwoNumbers(int a, int b)
     {
         return a + b;
+    }
+
+    private static int NumberOfDuplicatesInList(List<int> list)
+    {
+        Hashtable hashtable = new Hashtable();
+        foreach (int i in list)
+        {
+            if (hashtable.ContainsKey(i))
+            {
+                int value = (int)hashtable[i];
+                hashtable.Remove(i);
+                value++;
+                hashtable.Add(i, value);
+            }
+            else
+            {
+                hashtable.Add(i, 1);
+            }
+        }
+        int result = list.Count - hashtable.Count;
+        return result;
     }
 }
 
