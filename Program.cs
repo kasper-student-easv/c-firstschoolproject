71f4397207d884779f8fa0b752ff0bfdc4e9f35a
Kasper <kasand04@easv365.dk>
2026-08-24 10:46:27 +0200

    add hallo user

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //halloWriter();
+    }
+
+
+    private static void halloWriter()
+    {
+        Console.WriteLine("hallo:");
+        Console.WriteLine("user");
+    }
+}
+
